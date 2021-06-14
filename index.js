d663befdd139340a50e4efe89e82e8a101d31c46
bryancitu <bryanjesus.ra@gmail.com>
2021-06-14 03:17:57 -0500

    conociendo electron el objeto app y BrowserWindow

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const { app, BrowserWindow } = require('electron');
+
+app.on('ready', () => {
+    let win = new BrowserWindow()
+    
+    win.on('closed', () => {
+        win = null
+        app.quit()
+    })
+    
+})
+
+app.on('before-quit', () => {
+    console.log('Saliendo ...');
+})
