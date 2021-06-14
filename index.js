03e738eb104818b5a45d8f5f60ebed711b488b23
bryancitu <bryanjesus.ra@gmail.com>
2021-06-14 04:03:26 -0500

    personalizando nuestra ventana y el contenido

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 'use strict'
 
+// Iniciando los objetos app y BroserWindow
 const { app, BrowserWindow } = require('electron');
 
+// Ejecutando ordenes cuando la aplicacion esta lista
 app.on('ready', () => {
-    let win = new BrowserWindow()
+    // Creando una ventana
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: 'Hola Mundo',
+        center: true,
+        maximizable: false,
+        show: false
+    })
+
+    win.once('ready-to-show', () => {
+        win.show
+    })
+
+    //  cuando hacemos el evento de mover la ventana obtenemos la posicion
+    win.on('move', () => {
+        const position = win.getPosition()
+        console.log('La posicion de la ventana es:', position);
+    })
     
+    // Detectando el cierre de la ventana para cerrar el aplicativo
     win.on('closed', () => {
         win = null
         app.quit()
     })
+
+    // cargamos el contenido del la url a nuestra app desktop
+    win.loadURL('https://ego.pe/')
     
 })
 
+// Imprimiendo un mensaje en la consola antes de salir
 app.on('before-quit', () => {
     console.log('Saliendo ...');
 })
